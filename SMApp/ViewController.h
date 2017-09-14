42b3319b781aa4ff6484bdf095090113a8f731bd
家朋 <jiapeng@jiapengdeMacBook-Air.local>
2017-09-14 23:33:36 +0800

    Initial Commit

---
+++ SMApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SMApp
+//
+//  Created by 家朋 on 2017/9/14.
+//  Copyright © 2017年 家朋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SMApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SMApp
+//
+//  Created by 家朋 on 2017/9/14.
+//  Copyright © 2017年 家朋. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
